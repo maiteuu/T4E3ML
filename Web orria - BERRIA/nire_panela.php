87fa3c37892c7d80740d1da55911e2b6d44b3eaa
Markelag13 <markelag13@gmail.com>
2026-03-26 14:03:46 +0100

    Jokalairen panela hobetu eta admin paneleko funtzionalitate arazoa konpondu.

--- Web orria - BERRIA/nire_panela.php
+++ Web orria - BERRIA/nire_panela.php
@@ -2,78 +2,128 @@
 session_start();
 require_once 'includes/functions.php';
 
-// SEGURIDAD: Solo usuarios logueados con rol 'jokalari'
 if (!isset($_SESSION['rol']) || $_SESSION['rol'] !== 'jokalari') {
     header("Location: index.php");
     exit();
 }
 
+$tipoSemana = isset($_GET['mota']) ? $_GET['mota'] : ($_COOKIE['preferencia_semana'] ?? 'partido');
+if (isset($_GET['mota'])) {
+    setcookie("preferencia_semana", $tipoSemana, time() + (86400 * 30), "/");
+}
+
+$xmlPath = 'xml/planificacion.xml';
+if (!file_exists($xmlPath)) { die("Errorea: Ez da aurkitu '$xmlPath' fitxategia."); }
+
+$xml = simplexml_load_file($xmlPath);
+$resultado = $xml->xpath("//tipo[@id='$tipoSemana']");
+$plana = $resultado[0];
+
 $pageTitle = "Nire Panela";
 include 'includes/header.php';
-
-// Cargar los datos de seguimiento
-$seguimientoXML = simplexml_load_file('datos/seguimiento.xml');
-$usuarioActual = $_SESSION['usuario'];
-
-// Buscar el nodo del jugador actual usando XPath
-$misDatos = $seguimientoXML->xpath("//jugador[@nombre='$usuarioActual']");
 ?>
 
 <main class="w3-container w3-padding-32">
-    <div class="orri-titulua-container">
-        <h2 class="orri-titulua"><?php echo $usuarioActual ?>-(R)EN DIETA ETA ENTRENAMENDUA</h2>
-        <span class="orri-marra"></span>
-    </div>
+    <header class="orri-titulua-container w3-center w3-margin-bottom">
+        <h2 class="orri-titulua">NIRE PLANA: <?php echo strtoupper($plana['izena']); ?></h2>
+    </header>
 
-    <?php if ($misDatos): 
-        $datos = $misDatos[0]; ?>
-        
-        <div class="w3-row-padding">
-            <div class="w3-col m6 w3-margin-bottom">
-                <div class="w3-card-4 w3-white w3-round-large">
-                    <header class="w3-container w3-teal w3-round-large" style="border-bottom-left-radius:0; border-bottom-right-radius:0;">
-                        <h3><i class="fa fa-cutlery"></i> Asteko Dieta</h3>
-                    </header>
-                    <div class="w3-container w3-padding">
-                        <ul class="w3-ul w3-border-0">
-                            <?php foreach ($datos->dieta->comida as $comida): ?>
-                                <li class="w3-padding-16">
-                                    <span class="w3-tag w3-light-grey w3-text-teal w3-round"><b><?php echo $comida['tipo']; ?>:</b></span>
-                                    <span class="w3-margin-left"><?php echo $comida; ?></span>
-                                </li>
-                            <?php endforeach; ?>
-                        </ul>
-                        <div class="w3-panel w3-leftbar w3-sand w3-serif w3-margin-top">
-                            <p><i>"<?php echo $datos->dieta->oharrak; ?>"</i></p>
-                        </div>
-                    </div>
-                </div>
-            </div>
+    <section class="selector-semana-container">
+        <form method="GET" action="nire_panela.php" class="w3-card selector-card w3-padding w3-round-large">
+            <label class="w3-bold">Aukeratu aste mota: </label>
+            <select name="mota" class="w3-select w3-border-0 w3-white" style="width:auto; font-weight:bold; cursor:pointer;" onchange="this.form.submit()">
+                <option value="partido" <?= ($tipoSemana == 'partido') ? 'selected' : ''; ?>>Partidu Astea</option>
+                <option value="atseden" <?= ($tipoSemana == 'atseden') ? 'selected' : ''; ?>>Atseden Astea</option>
+            </select>
+        </form>
+    </section>
 
-            <div class="w3-col m6">
-                <div class="w3-card-4 w3-white w3-round-large">
-                    <header class="w3-container w3-blue-grey w3-round-large" style="border-bottom-left-radius:0; border-bottom-right-radius:0;">
-                        <h3><i class="fa fa-bicycle"></i> Lan Plana</h3>
-                    </header>
-                    <div class="w3-container w3-padding">
-                        <table class="w3-table w3-striped">
-                            <?php foreach ($datos->entrenamendua->saioa as $saioa): ?>
-                                <tr>
-                                    <td style="width: 30%;"><b><?php echo $saioa['egun']; ?></b></td>
-                                    <td><?php echo $saioa; ?></td>
-                                </tr>
-                            <?php endforeach; ?>
-                        </table>
-                    </div>
-                </div>
-            </div>
+    <article class="w3-card-4 w3-white w3-round-large w3-margin-bottom" style="overflow:hidden">
+        <header class="w3-container" style="background-color: #871521; color: white;">
+            <h4><i class="fa fa-calendar"></i> ASTEKO ENTRENAMENDUAK</h4>
+        </header>
+        <div class="w3-responsive">
+            <table class="w3-table tabla-plan">
+                <thead>
+                    <tr class="w3-dark-grey">
+                        <th>EGUNA</th>
+                        <th>GOIZA</th>
+                        <th>ARRATSALDEA</th>
+                        <th>INTENTSITATEA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($plana->eguna as $eguna): 
+                        // Lógica de detección: SÁBADO y semana de PARTIDO
+                        $esPartidoReal = ($tipoSemana == 'partido' && (string)$eguna['izena'] == 'Larunbata');
+                    ?>
+                    <tr class="<?= $esPartidoReal ? 'fila-partidu-ofiziala' : ''; ?>">
+                        <td class="col-eguna"><?= $eguna['izena']; ?></td>
+                        
+                        <td class="w3-small celda-info">
+                            <?php if (mb_stripos($eguna->goizez, 'Atseden') !== false && mb_stripos($eguna->goizez, 'Gimnasioa') === false): ?>
+                                <span class="txt-grande color-atseden">ATSEDENA</span>
+                            <?php else: ?>
+                                <span class="label-mota label-gym"><i class="fa fa-dumbbell"></i> Gimnasioa</span>
+                                <?= $eguna->goizez; ?>
+                            <?php endif; ?>
+                        </td>
+
+                        <td class="w3-small celda-info">
+                            <?php if ($esPartidoReal): ?>
+                                <span class="txt-grande color-partidu"><i class="fa fa-star"></i> PARTIDUA <i class="fa fa-star"></i></span>
+                                <div class="w3-center"><?= $eguna->arratsaldez; ?></div>
+                            <?php elseif (mb_stripos($eguna->arratsaldez, 'Atsedena') !== false && mb_stripos($eguna->arratsaldez, 'Pista') === false): ?>
+                                <span class="txt-grande color-atseden">ATSEDENA</span>
+                            <?php else: ?>
+                                <span class="label-mota label-pista"><i class="fa fa-futbol-o"></i> Pista</span>
+                                <?= $eguna->arratsaldez; ?>
+                            <?php endif; ?>
+                        </td>
+
+                        <td class="w3-center w3-v-align" style="vertical-align: middle;">
+                            <span class="w3-tag w3-round w3-orange w3-small w3-block"><?= $eguna->intentsitatea; ?></span>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
+    </article>
 
-    <?php else: ?>
-        <div class="w3-panel w3-pale-red w3-border w3-round w3-center">
-            <p>Oraindik ez daukazu dieta edo entrenamendu planik esleituta. Jarri harremanetan administratzailearekin.</p>
+    <article class="w3-card-4 w3-white w3-round-large" style="overflow:hidden">
+        <header class="w3-container w3-teal">
+            <h4><i class="fa fa-cutlery"></i> NUTRIZIO PLANA</h4>
+        </header>
+        <div class="w3-responsive">
+            <table class="w3-table tabla-plan">
+                <thead>
+                    <tr style="background-color: #00796B; color: white;">
+                        <th style="width: 110px;">EGUNA</th>
+                        <th>GOSARIA</th>
+                        <th>BAZKARIA</th>
+                        <th>ASKARIA</th>
+                        <th>AFARIA</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($plana->eguna as $eguna): 
+                        $esPartidoReal = ($tipoSemana == 'partido' && (string)$eguna['izena'] == 'Larunbata');
+                    ?>
+                    <tr class="<?= $esPartidoReal ? 'fila-partidu-ofiziala' : ''; ?>">
+                        <td class="col-eguna"><?= $eguna['izena']; ?></td>
+                        <?php foreach (['gosaria', 'bazkaria', 'askaria', 'afaria'] as $comida): ?>
+                            <td class="w3-small celda-info">
+                                <span class="label-mota label-dieta"><?= ucfirst($comida); ?></span>
+                                <?= $eguna->dieta->$comida; ?>
+                            </td>
+                        <?php endforeach; ?>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
         </div>
-    <?php endif; ?>
+    </article>
 </main>
 
 <?php include 'includes/footer.php'; ?>
