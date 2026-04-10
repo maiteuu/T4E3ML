7438f1bd8ac6c25d458edaaf061e6230af02d87a
Oier Marañon <o.maranonocejo@ikasle.eus>
2026-04-10 21:38:47 +0200

    logina aldatuta erabiltzaileak.xml-ra berriro guztia ez refaktorizatzeko

--- Web orria - BERRIA/login.php
+++ Web orria - BERRIA/login.php
@@ -7,10 +7,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $passIn = $_POST['pass'];
 
     // Cargar usuarios desde el XML
-    $usuariosXML = simplexml_load_file('xml/federazioa.xml');
+    $usuariosXML = simplexml_load_file('datos/erabiltzaileak.xml');
     $encontrado = false;
 
-    foreach ($usuariosXML->Erabiltzaileak->Erabiltzailea as $u) {
+    foreach ($usuariosXML->Erabiltzailea as $u) {
         if ($u->izena == $userIn && $u->password == $passIn) {
             $_SESSION['erabiltzailea'] = (string)$u->izena;
             $_SESSION['rol'] = (string)$u->rol;
